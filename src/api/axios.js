6a9528590525eaa4a1cea142f7d402d1e9612bd5
Keerthana9025 <keerthpalani18@gmail.com>
2026-05-31 15:03:20 +0530

    fix CORS with correct frontend URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hospital-backendflow.onrender.com/api",
+  baseURL: "https://hospital-backendflow.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
