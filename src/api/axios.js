f3ba9ca71132e60cde0080c47b113076d0de3772
Keerthana9025 <keerthpalani18@gmail.com>
2026-05-31 14:42:51 +0530

    fix backend URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hospital-backendflow.onrender.com",
+  baseURL: "https://hospital-backendflow.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
 
